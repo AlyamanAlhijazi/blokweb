f377eae7a90d96bd5cb0616b70aa7ed6e7336c4f
AlyamanAlhijazi <145450519+AlyamanAlhijazi@users.noreply.github.com>
2024-12-05 13:55:39 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -28,3 +28,21 @@ tabs.forEach(tab => {
     });
 });
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuButton = document.getElementById("menu-button");
+    const mainMenu = document.getElementById("main-menu");
+
+    menuButton.addEventListener("click", () => {
+        const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
+
+        menuButton.setAttribute("aria-expanded", !isExpanded);
+        mainMenu.hidden = isExpanded;
+
+        // Verander de aria-label voor toegankelijkheid
+        menuButton.setAttribute("aria-label", isExpanded ? "Open menu" : "Close menu");
+        console.log(mainMenu.hidden); // Dit moet wisselen tussen true/false als je op de knop drukt
+
+    });
+});
+
