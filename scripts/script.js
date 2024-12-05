7f5c8fae3af0c54a15f90692ee94fb740f4e63a9
AlyamanAlhijazi <145450519+AlyamanAlhijazi@users.noreply.github.com>
2024-12-05 14:26:41 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -9,21 +9,20 @@ const tabContents = document.querySelectorAll('[data-tab-content]');
 
 tabs.forEach(tab => {
     tab.addEventListener('click', (e) => {
-        e.preventDefault(); // Stop standaard link gedrag
+        e.preventDefault(); 
 
-        // Verwijder 'active' van alle tab-content
         tabContents.forEach(content => content.classList.remove('active'));
 
-        // // Verwijder 'active' van alle tab-links (optioneel)
+    
         tabs.forEach(t => t.classList.remove('active'));
 
-        // Vind het doel-element via data-tab-target
+        
         const target = document.querySelector(tab.dataset.tabTarget);
 
-        // Controleer of het doel-element bestaat
+        
         if (target) {
-            target.classList.add('active'); // Voeg 'active' toe aan het doel
-            tab.classList.add('active');   // Voeg 'active' toe aan de link 
+            target.classList.add('active'); 
+            tab.classList.add('active');   
         }
     });
 });
@@ -39,9 +38,9 @@ document.addEventListener("DOMContentLoaded", () => {
         menuButton.setAttribute("aria-expanded", !isExpanded);
         mainMenu.hidden = isExpanded;
 
-        // Verander de aria-label voor toegankelijkheid
+        
         menuButton.setAttribute("aria-label", isExpanded ? "Open menu" : "Close menu");
-        console.log(mainMenu.hidden); // Dit moet wisselen tussen true/false als je op de knop drukt
+        console.log(mainMenu.hidden); 
 
     });
 });
